9ced82b68d82aa257dbfc6c647133922fa9d92e5
milindl <imilindluthra@gmail.com>
2017-05-06 14:27:31 +0530

    Add basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170506084436) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "cclogin"
+    t.decimal "rollno"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
