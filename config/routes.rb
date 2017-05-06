d7754fea9af6e0d1585dd5eaf46f12ab6894dd44
milindl <imilindluthra@gmail.com>
2017-05-06 13:02:23 +0530

    Add static page help and associated tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get '/help', to: 'static_pages#help'
+  root 'static_pages#help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
