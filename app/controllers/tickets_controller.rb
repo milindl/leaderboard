6888d97ae0254e1bcf1ae1851bfbc7197b516a7e
milindl <imilindluthra@gmail.com>
2017-05-07 16:01:00 +0530

    Add create action to tickets controller, make form for adding new ticket
    This action associates a new ticket with the given user, and creates a user if
    user does not exist.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,6 +5,38 @@ class TicketsController < ApplicationController
   def view
   end
 
+  def create
+    cclogin = params[:cclogin]
+    ccpassword = params[:ccpassword]
+    a_user = User.find_by(cclogin: cclogin)
+    if a_user.nil?
+      a_user = User.create(cclogin: cclogin, password: ccpassword)
+    else
+      a_user.password = ccpassword
+    end
+    if a_user.is_valid_login? != true
+      flash[:error] = "Ticket could not be created. Check your credentials."
+      return redirect_to tickets_new_url(error_params)
+    end
+    if a_user.tickets.create(ticket_params).id == nil
+      flash[:error] = "Ticket could not be created. Check your ticket data."
+      return redirect_to tickets_new_url(error_params)
+    end
+    flash[:success] = "Ticket created successfully. It is pending approval."
+    redirect_to root_path
+  end
+
   def leaderboard
   end
+
+
+  private
+  def ticket_params
+    params.permit(:points, :desc, :link, :typeof)
+  end
+
+  def error_params
+    params.permit(:points, :desc, :link, :typeof, :cclogin)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'tickets/view'
 
   get 'tickets/leaderboard'
-
+  post 'tickets/create'
   get '/help', to: 'static_pages#help'
-  root 'static_pages#help'
+  root 'tickets#leaderboard'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
