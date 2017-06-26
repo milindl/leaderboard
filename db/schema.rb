e213572fe1a9470725a6039a7088328463ed3759
Anant Prakash <anantprakashjsr@gmail.com>
2017-06-27 02:53:19 +0530

    prevent addition of duplicate pending tickets #6
    - adds uniqueness validation for `ticket.link` in app/models/ticket.rb
    - adds migration for database uniqueness validation for `ticket.link`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170620055231) do
+ActiveRecord::Schema.define(version: 20170626214846) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string "username"
--- test/models/ticket_test.rb
+++ test/models/ticket_test.rb
@@ -4,11 +4,11 @@ class TicketTest < ActiveSupport::TestCase
   def setup
     set_up_fake_network
     @user = User.create(cclogin: valid_user, password: valid_password)
-    @ticket = @user.tickets.new
-    @ticket.link = "http://valid.site.com/"
-    @ticket.desc = "A good ticket"
-    @ticket.typeof = "ML"
-    @ticket.points = 3
+    @ticket_details = { link: "http://valid.site.com/",
+                       points: 3,
+                       desc: "",
+                       typeof: "ML" }
+    @ticket = @user.tickets.new(@ticket_details)
   end
 
   test "invalid links on the ticket should prevent creation" do
@@ -33,4 +33,27 @@ class TicketTest < ActiveSupport::TestCase
     assert @ticket.pending?
   end
 
+  test "should not add duplicate Pending ticket" do
+    @ticket.save
+    duplicate_ticket = @user.tickets.new(@ticket_details)
+    assert_not duplicate_ticket.save
+  end
+
+  test "should add duplicate even if approved ticket exists" do
+    @ticket.save
+    t = Ticket.find_by(id: @ticket.id)
+    t.update_column(:status, :approved)
+
+    duplicate_ticket = @user.tickets.new(@ticket_details)
+    assert duplicate_ticket.save
+  end
+
+  test "should add duplicate even if deleted ticket exists" do
+    @ticket.save
+    t = Ticket.find_by(id: @ticket.id)
+    t.update_column(:status, :deleted)
+
+    duplicate_ticket = @user.tickets.new(@ticket_details)
+    assert duplicate_ticket.save
+  end
 end
